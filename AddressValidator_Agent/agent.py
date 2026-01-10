fa052d54e70cfa26fba2e27dfa6ea0ac28cd80ed
rajesh ravindran <rajeshravindran05@gmail.com>
2026-01-10 08:46:21 +0000

    test

--- AddressValidator_Agent/agent.py
+++ AddressValidator_Agent/agent.py
@@ -1,23 +1,65 @@
 from typing import override
 from google.adk.agents.llm_agent import Agent
 from dotenv import load_dotenv
+import datetime
+from zoneinfo import ZoneInfo
 
 load_dotenv(override=True)
 
-def get_current_time(city:str)->dict:
+def get_current_time(city: str) -> dict:
+    """Returns the current time in a specified city.
+
+    Args:
+        city (str): The name of the city for which to retrieve the current time.
+
+    Returns:
+        dict: status and result or error msg.
     """
-    Returns the current time in a specified city 
+
+    if city.lower() == "new york":
+        tz_identifier = "America/New_York"
+    else:
+        return {
+            "status": "error",
+            "error_message": (
+                f"Sorry, I don't have timezone information for {city}."
+            ),
+        }
+
+    tz = ZoneInfo(tz_identifier)
+    now = datetime.datetime.now(tz)
+    report = (
+        f'The current time in {city} is {now.strftime("%Y-%m-%d %H:%M:%S %Z%z")}'
+    )
+    return {"status": "success", "report": report}
+
+def get_weather(city:str)->dict:
+    """Retrieves the current weather report for a specified city.
+
+    Args:
+        city (str): The name of the city for which to retrieve the weather report.
+
+    Returns:
+        dict: status and result or error msg.
     """
-    return {
-        "status":"success",
-        "city":city,
-        "time":"10:30 AM"
-    }
+    if city.lower() == "new york":
+        return {
+            "status": "success",
+            "report": (
+                "The weather in New York is sunny with a temperature of 25 degrees"
+                " Celsius (77 degrees Fahrenheit)."
+            ),
+        }
+    else:
+        return {
+            "status": "error",
+            "error_message": f"Weather information for '{city}' is not available.",
+        }
 
 root_agent = Agent(
     model='gemini-2.5-flash',
     name='AddressValidator_Agent',
-    description='Tells the current time in a specified city ',
-    instruction='You are a helpful agent which tells the time',
-    tools=[get_current_time]
+    description='Agent to answer questions about the time and weather in a city.',
+    instruction='You are a helpful agent who can answer user questions about the time and weather in a city.',
+    tools=[get_current_time, get_weather]
 )
