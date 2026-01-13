81fa6274df9b265ce9e296e4707169302db00d17
rajesh ravindran <rajeshravindran05@gmail.com>
2026-01-13 21:25:26 +0000

    updated

--- LBG_IPI_DQ_CHECKS/agents/document_pensions_agent.py
+++ LBG_IPI_DQ_CHECKS/agents/document_pensions_agent.py
@@ -1,6 +1,6 @@
 from google.adk.agents import LlmAgent
 
-document_pension_agent=LlmAgent(
+document_pensions_agent=LlmAgent(
     name='document_pension_agent',
     description="document_pension_agent",
     instruction="""
